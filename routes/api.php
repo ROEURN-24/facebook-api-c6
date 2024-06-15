4b8b64869cbd912e71419aa22cb0e00eef8d3fa6
ROEURNKAKI <roeurnkaki@gmail.com>
2024-06-15 17:59:08 +0700

    #4 [feature] Authentications user

--- routes/api.php
+++ routes/api.php
@@ -36,3 +36,50 @@ Route::prefix('like')->group(function () {
     Route::post('/create', [LikeController::class, 'store'])->name('like.create');
     Route::get('/show/{id}', [LikeController::class, 'show'])->name('like.show');
 });
+
+
+
+
+// Authentication routes
+Route::post('/register', [AuthController::class, 'createUser']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Password routes
+Route::post('/forget-password', [PasswordController::class, 'forgetPassword']);
+Route::post('/reset-password', [PasswordController::class, 'resetPassword']);
+Route::put('/change-password', [PasswordController::class, 'changePassword']);
+
+// Protected routes (require authentication)
+Route::middleware('auth:sanctum')->group(function () {
+    // Authentication routes
+    Route::get('/user', [AuthController::class, 'index']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // User routes
+    Route::put('/user', [UserController::class, 'updateUser']);
+    Route::delete('/user', [UserController::class, 'deleteUser']);
+    Route::post('/upload-images', [UserController::class, 'uploadImages']);
+});
+
+
+
+Route::get('/storage/{path}', function ($path) {
+    // Construct the full file path
+    $filePath = storage_path('app/public/' . $path);
+
+    // Check if the file exists
+    if (!File::exists($filePath)) {
+        abort(404);
+    }
+
+    // Determine the MIME type of the file
+    $mime = File::mimeType($filePath);
+
+    // Set appropriate headers for the response
+    $headers = [
+        'Content-Type' => $mime,
+    ];
+
+    // Return the file as a response
+    return response()->file($filePath, $headers);
+})->where('path', '.*');
